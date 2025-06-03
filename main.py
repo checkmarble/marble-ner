8de2edec205c4f0c4c7f6fc38fe60f99a4e99cde
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-06-03 12:55:28 +0200

    Upgrade dependencies.

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
+from dataclasses import dataclass, field
 import os
 
-from typing import Annotated, Optional
+import uvicorn
 
-from fastapi import Depends, FastAPI, Request, Response
+from typing import Annotated, List
+
+from fastapi import Depends, FastAPI, Response
 from fastapi.security import OAuth2PasswordBearer
 from gliner import GLiNER
 from pydantic import BaseModel
+from functools import lru_cache
 
 if os.getenv('NER_API_KEY') is None:
     raise Exception('NER_API_KEY must be defined')
 
 MODEL=os.getenv("GLINER_MODEL", "urchade/gliner_medium-v2.1")
 LABELS=os.getenv("GLINER_LABELS", "Person,Company").split(",")
+
+@dataclass
+class Settings():
+    labels: List[str] = field(default_factory=lambda: LABELS)
+
+@lru_cache
+def settings():
+    return Settings()
 
 class Query(BaseModel):
   text: str
@@ -29,14 +41,14 @@ def healthcheck():
   pass
 
 @app.post("/detect")
-def detect(token: Annotated[str, Depends(auth)], query: Query, response: Response):
+def detect(settings: Annotated[Settings, Depends(settings)], token: Annotated[str, Depends(auth)], query: Query, response: Response):
   if token != os.environ['NER_API_KEY']:
     response.status_code = 401
 
     return {"detail": "Not authenticated"}
 
-  labels = LABELS
-  entities = model.predict_entities(query.text, labels, threshold=0.1)
+  print(settings.labels)
+  entities = model.predict_entities(query.text, settings.labels, threshold=0.1)
 
   matches = []
 
@@ -45,3 +57,6 @@ def detect(token: Annotated[str, Depends(auth)], query: Query, response: Respons
     matches.append(match)
 
   return matches
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", "9000")))
--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ import os
 os.environ['NER_API_KEY'] = 'randomapikey'
 
 from fastapi.testclient import TestClient
-from main import app
+from main import Settings, app, settings
 
 
 client = TestClient(app)
@@ -23,6 +23,39 @@ def test_detection():
   assert len(json) == 1
   assert json[0]['type'] == "Person"
   assert json[0]['text'] == "joe finnigan"
+
+def test_detection_for_extra_type():
+  def _settings():
+      return Settings(labels=['Person', 'Country'])
+
+  app.dependency_overrides[settings] = _settings
+
+  response = client.post('/detect', headers={'authorization': 'Bearer randomapikey'}, json={"text": "dinner with joe finnigan at ACME Inc in Cyprus"})
+
+  assert response.status_code == 200
+
+  json = response.json()
+
+  assert len(json) == 2
+  assert json[0]['type'] == "Person"
+  assert json[0]['text'] == "joe finnigan"
+  assert json[1]['type'] == "Country"
+  assert json[1]['text'] == "Cyprus"
+
+  app.dependency_overrides = {}
+
+def test_detection_for_defaults():
+  response = client.post('/detect', headers={'authorization': 'Bearer randomapikey'}, json={"text": "dinner with joe finnigan at ACME Inc"})
+
+  assert response.status_code == 200
+
+  json = response.json()
+
+  assert len(json) == 2
+  assert json[0]['type'] == "Person"
+  assert json[0]['text'] == "joe finnigan"
+  assert json[1]['type'] == "Company"
+  assert json[1]['text'] == "ACME Inc"
 
 def test_invalid_bearer_token():
   response = client.post('/detect', headers={'authorization': 'Bearer wrongapikey'}, json={"text": "dinner with joe finnigan"})
